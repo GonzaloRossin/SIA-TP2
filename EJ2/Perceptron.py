fix weight calculation error

EJ2/Perceptron.py
@@ -17,21 +17,21 @@ class Perceptron:
         self.utils = utils
         self.activationType = activationType
 
-    def calculateO(self, inputSum, activationType, beta):
+    def calculateO(self, h, activationType, beta):
         if activationType == ActivationType.ActivationType.LINEAR:
-            return inputSum
+            return h
         elif activationType == ActivationType.ActivationType.SIGMOID:
-            return math.tanh(beta * inputSum)
+            return math.tanh(beta * h)
 
     def calculateError(self, inputMatrix, weightVector, activationType, beta):
         error = 0
         for i in range(inputMatrix.shape[0]):
-            result = self.resultVector[i]
+            result = self.resultVector[i][0]
             if activationType == ActivationType.ActivationType.SIGMOID:
                 result = self.normalize(result)
             h = np.dot(inputMatrix[i], weightVector)
             error += (result - self.calculateO(h, activationType, beta)) ** 2
-        return 0.5 * error[0]
+        return 0.5 * error
 
     def normalize(self, result):
         return (2 * (result - np.min(self.resultVector)) / (np.max(self.resultVector) - np.min(self.resultVector))) - 1
@@ -40,7 +40,7 @@ class Perceptron:
         if ActivationType.ActivationType.LINEAR == self.activationType:
             return 1
         elif ActivationType.ActivationType.SIGMOID == self.activationType:
-            return beta * (1 - O)
+            return beta * (1 - O**2)
 
     def calculateWeights(self, activationType, weight_vector, beta, N):
         sumResultVector = []
@@ -49,12 +49,14 @@ class Perceptron:
             for i in range(self.trainingInput.shape[0]):
                 result = self.resultVector[i]
                 h = np.dot(self.trainingInput[i], weight_vector)
-                gDiff = self.evaluateGdiff(self.calculateO(h, self.activationType, beta), beta)
+                O = self.calculateO(h, self.activationType, beta)
+                gDiff = self.evaluateGdiff(O, beta)
                 Ei = self.trainingInput[i][j]
                 if activationType == ActivationType.ActivationType.SIGMOID:
-                    sumResult += (self.normalize(result) - self.calculateO(h, self.activationType, beta) * gDiff * Ei)
+                    sumResult += ((self.normalize(result) - O) * gDiff * Ei)
                 else:
-                    sumResult += (self.normalize(result) - self.calculateO(h, self.activationType, beta) * gDiff * Ei)
+                    sumResult += ((result - O) * gDiff * Ei)
+
             sumResultVector.append(sumResult[0])
 
         for i in range(len(weight_vector)):
@@ -79,7 +81,6 @@ class Perceptron:
         w = weight_vector
         error_min = 1000000
         w_min = None
-        # self.getMinMaxValues(self.inputMatrix)
         if selectionType == SelectionType.SelectionType.RANDOM:
             while error_min > 0 and i < upper_limit:
                 pickInput = random.choice(self.inputMatrix)
@@ -96,9 +97,8 @@ class Perceptron:
 
                 np.random.shuffle(self.inputMatrix)
                 self.trainingInput, self.resultVector = self.utils.splitInputFromResult(self.inputMatrix)
-                self.calculateWeights(self.activationType, w, beta, N)
+                w = self.calculateWeights(self.activationType, w, beta, N)
                 error = self.calculateError(self.trainingInput, w, self.activationType, beta)
-                wOverTime.append(w)
                 errorVsT.append(error)
                 if error < error_min:
                     error_min = error