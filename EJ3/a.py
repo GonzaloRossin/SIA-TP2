Fixed bugs, normalization for tanh

EJ3/a.py
@@ -5,7 +5,7 @@ from utils.constants import LOGISTIC
 from utils.InputHandler import InputHandler
 from multilayer_utils.Predictions import predict
 from multilayer_utils.MultilayerPerceptron import multilayer_perceptron
-from multilayer_utils.Normalization import denormalize_sigmoid
+from multilayer_utils.Normalization import denormalize
 
 def ejA_main():
 
@@ -42,25 +42,25 @@ def ejA_main():
     '''
     
     if (input_handler.normalize):
-        print(f"Expected = {np.squeeze(denormalize_sigmoid(test_Y, input_handler.min_y, input_handler.max_y))}\n")
-        print(f"Denormalized Output = {np.squeeze(denormalize_sigmoid(O, input_handler.min_y, input_handler.max_y))}\n")
+        print(f"Expected = {np.squeeze(denormalize(test_Y, input_handler.min_y, input_handler.max_y, input_handler.output_activation))}\n")
+        print(f"Denormalized Output = {np.squeeze(denormalize(O, input_handler.min_y, input_handler.max_y, input_handler.output_activation))}\n")
     else:
         print(f"Expected = {np.squeeze(test_Y)}\n")
         print(f"Output = {np.squeeze(O)}\n")
 
     '''
     print(f"Trained parameters\n {parameters}\n")
+    '''
 
     # Graphics
     fig = plt.subplot()
     fig.set_title("Error function")
     fig.set_xlabel("Epochs")
     fig.set_ylabel("Error")
-    fig.set_ylim([0, 1])
+    #fig.set_ylim([0, 1])
     #fig.plot(errors, marker='o')
     fig.plot(errors)
     plt.show()
-    '''
 
     # TODO: Output graphics
 

EJ3/b.py
@@ -1,11 +1,10 @@
 import json
 import numpy as np
 import matplotlib.pyplot as plt
-from utils.constants import LOGISTIC
 from utils.InputHandler import InputHandler
 from multilayer_utils.Predictions import predict
 from multilayer_utils.MultilayerPerceptron import multilayer_perceptron
-from multilayer_utils.Normalization import denormalize_sigmoid
+from multilayer_utils.Normalization import denormalize
 
 def ejB_main():
 
@@ -43,9 +42,8 @@ def ejB_main():
     '''
     
     if (input_handler.normalize):
-        print(f"Expected = {np.squeeze(denormalize_sigmoid(test_Y, input_handler.min_y, input_handler.max_y))}\n")
-        print(f"Output = {np.squeeze(O)}\n")
-        print(f"Denormalized Output = {np.squeeze(denormalize_sigmoid(O, input_handler.min_y, input_handler.max_y))}\n")
+        print(f"Expected = {np.squeeze(denormalize(test_Y, input_handler.min_y, input_handler.max_y, input_handler.output_activation))}\n")
+        print(f"Denormalized Output = {np.squeeze(denormalize(O, input_handler.min_y, input_handler.max_y, input_handler.output_activation))}\n")
     else:
         print(f"Expected = {np.squeeze(test_Y)}\n")
         print(f"Output = {np.squeeze(O)}\n")

EJ3/multilayer_utils/error.py
@@ -7,8 +7,7 @@ def linear_regression_loss(O, Y):
 
 def logistic_regression_loss(O, Y):
     D = 1-O
-    #return - np.dot(Y,np.log(O).T) - np.dot(1-Y,np.log(1-O).T)
-    return - np.dot(Y,np.log(O)) - np.dot(1-Y,np.log(D))
+    return - np.dot(Y,np.log(O).T) - np.dot(1-Y,np.log(1-O).T)
 
 def tanh_loss(O, Y):
     loss_pos_1 = np.dot((1+Y).T, np.log(1+Y) - np.log(1+O))

EJ3/utils/InputHandler.py
@@ -1,6 +1,6 @@
 import numpy as np
 from utils.constants import LOGISTIC
-from multilayer_utils.Normalization import normalize_sigmoid
+from multilayer_utils.Normalization import normalize
 
 class InputHandler:
     
@@ -41,7 +41,7 @@ class InputHandler:
 
         X, Y = self.read_input(input['input_file'], num_features)
         if (self.normalize):
-            Y, self.min_y, self.max_y = normalize_sigmoid(Y)
+            Y, self.min_y, self.max_y = normalize(Y, self.output_activation)
 
         self.ratio = input['training_set_ratio']
         training_idx = len(X) * self.ratio // 100
@@ -52,8 +52,8 @@ class InputHandler:
 
         self.training_set_X = np.array(train_X).T
         self.test_set_X = np.array(test_X).T
-        self.training_set_Y = np.array(train_Y)
-        self.test_set_Y = np.array(test_Y)
+        self.training_set_Y = np.array(train_Y).T
+        self.test_set_Y = np.array(test_Y).T
         
         if (input['hidden_layers']['use_num']):
             self.layers_dim = self.random_layers_dim(num_features, num_outputs, input['hidden_layers']['num_layers'], input['hidden_layers']['max_dim'])